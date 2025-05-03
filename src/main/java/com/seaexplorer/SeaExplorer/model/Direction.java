029503e429fa0ea3f4037003d5bae12b6e529984
Mayank Golani <mayankgolani0929@gmail.com>
2025-05-03 17:08:19 +0530

    add direction turning logic

--- src/main/java/com/seaexplorer/SeaExplorer/model/Direction.java
+++ src/main/java/com/seaexplorer/SeaExplorer/model/Direction.java
@@ -1,5 +1,23 @@
 package com.seaexplorer.SeaExplorer.model;
 
 public enum Direction {
-    N, E, S, W
+    N, E, S, W;
+
+    public Direction turnLeft() {
+        return switch (this) {
+            case N -> W;
+            case W -> S;
+            case S -> E;
+            case E -> N;
+        };
+    }
+
+    public Direction turnRight() {
+        return switch (this) {
+            case N -> E;
+            case E -> S;
+            case S -> W;
+            case W -> N;
+        };
+    }
 }
