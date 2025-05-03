30d8cf58258eab5bda95d49002ac1c20c72fb188
Mayank Golani <mayankgolani0929@gmail.com>
2025-05-03 16:43:54 +0530

    Initial Commit

---
+++ src/main/java/com/seaexplorer/SeaExplorer/SeaExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.seaexplorer.SeaExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaExplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seaexplorer/SeaExplorer/SeaExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seaexplorer.SeaExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
